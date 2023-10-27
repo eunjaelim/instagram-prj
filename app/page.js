3c2ec4bd8dfc9b8fb17870f70bab2b522ee3cb51
eunjaelim <imeunjae361@gmail.com>
2023-10-27 09:17:33 +0900

    배포다시

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>안녕하세요.</div>
+      배포를 다시 했습니다. by ablearn
     </main>
   );
 }
